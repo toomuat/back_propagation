316a2988efa95e0fdb5632528e40c1fc4d9f469b
tom_in_wsl <32962031+toomuat@users.noreply.github.com>
2021-04-29 18:30:53 +0900

    commit

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
 use ndarray::prelude::*;
 use rand::Rng;
 use std::fs::File;
+#[allow(unused_imports)]
 use std::io::{BufRead, BufReader, Error, Write};
 
 fn main() -> std::io::Result<()> {
@@ -50,41 +51,37 @@ fn main() -> std::io::Result<()> {
     let data_arr2: Array2<f64> = data.iter().map(|v| [v[0], v[1]]).collect::<Vec<_>>().into();
     let data_rev = data_arr2.reversed_axes();
 
-    let eta = 0.01;
+    let eta = 0.1;
 
     let times = 1000;
 
     for _ in 0..times {
-        let u = mat_w_in_to_mid.dot(&data_rev); // (3, 2) * (2, n) = (3, n)
+        // println!("{}", mat_w_in_to_mid);
+        let u = mat_w_in_to_mid.dot(&data_rev); // (3, 2) * (2, n)
+        // println!("{}", u);
         let y = u.map(|i| sigmod(*i)); // (3, n)
-        let v = mat_w_mid_to_out.dot(&y); // (1, 3) * (3, n) = (1, n)
+        // println!("{}", y);
+        let v = mat_w_mid_to_out.dot(&y); // (1, 3) * (3, n)
         let z = v.map(|i| sigmod(*i)); // (1, n)
-
-        // let J = (v - &train_mat).fold(0., |sum, i| sum + i.powf(2.)) as f64 / 2.;
-        // let J = (v - &train_mat).map(|a| a).fold(0., |sum, i| sum + (**i).powf(2.)) as f64 / 2.;
 
         let data_rev2 = data_rev.to_owned().reversed_axes();
 
+        let j = &train_mat - z; // ∂J/∂z (1, n)
+
         mat_w_mid_to_out = // (1, 3)
-            mat_w_mid_to_out + eta * ((&train_mat - &z) * v.map(|i| sigmod_diff(*i))).dot(&y.reversed_axes());
+            mat_w_mid_to_out
+            + eta * (&j * v.map(|i| sigmod_diff(*i)))
+            .dot(&y.reversed_axes()); // (1, n) * (n, 3)
 
         let mat_w_mid_to_out2 = mat_w_mid_to_out.to_owned().reversed_axes();
 
         mat_w_in_to_mid = mat_w_in_to_mid // (3, 2)
             + eta
                 * (&mat_w_mid_to_out2.dot( // (3, 1)
-                    &((&train_mat - &z) * v.map(|i| sigmod_diff(*i))) // (1, n)
+                    &(&j * v.map(|i| sigmod_diff(*i))) // (1, n)
                 )
-                * u.map(|i| sigmod_diff(*i)))
+                * u.map(|i| sigmod_diff(*i)))  // (3, n)
                 .dot(&data_rev2); // (n, 2)
-
-        // mat_w_in_to_mid = mat_w_in_to_mid
-        //     + eta
-        //         * (&mat_w_mid_to_out
-        //             * (&train_mat - &z)
-        //             * v.map(|i| sigmod_diff(*i))
-        //             * u.map(|i| sigmod_diff(*i)))
-        //         * &data_rev;
     }
 
     let filename2 = "result.txt";
@@ -94,20 +91,13 @@ fn main() -> std::io::Result<()> {
         let x1 = rng.gen_range(-2.0..2.0);
         let x2 = rng.gen_range(-2.0..2.0);
         let x = Array::from_shape_vec((2, 1), vec![x1, x2]).unwrap();
-        let u = mat_w_in_to_mid.dot(&x);
-        let y = u.map(|i| sigmod(*i));
-        let v = mat_w_mid_to_out.dot(&y);
-        let z = v.map(|i| sigmod(*i));
-        // let z = v.map(|i| sigmod(*i)).collect::<f64>().into();
 
-        // println!("{:?}", z);
-        // println!("{}", z[[0,0]]);
-        // assert_eq!(z.shape(), &[1,1]);
-        // println!("{}", z[0][0]); // error
-        // println!("{:?}", z.shape()); // [1,1]
-        // println!("{}", z[0]); // error
+        let u = mat_w_in_to_mid.dot(&x); // (3, 2) * (2, 1)
+        let y = u.map(|i| sigmod(*i)); // (3, 1)
+        let v = mat_w_mid_to_out.dot(&y); // (1, 3) * (3, 1)
+        let z = v.map(|i| sigmod(*i)); // (1, 1)
 
-        write!(file, "{}\n", format!("{} {} {}", x1, x2, z[[0,0]]))?;
+        write!(file, "{}\n", format!("{} {} {}", x1, x2, z[[0, 0]]))?;
     }
 
     Ok(())
@@ -117,6 +107,7 @@ fn func(x1: f64, x2: f64) -> f64 {
     (x1.powf(2.0) + x2.powf(2.0)).sqrt()
 }
 
+#[allow(dead_code)]
 fn print_type_of<T>(_: &T) {
     println!("{}", std::any::type_name::<T>())
 }
